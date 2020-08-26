8c45fb5e1220d78e77744d5f3aede7a1291a5888
randomizerkatt <ABroderick@student.neumont.edu>
2020-08-26 12:55:36 -0600

    Update MyHeader.php

--- frontend/MyHeader.php
+++ frontend/MyHeader.php
@@ -6,3 +6,9 @@
     <link href="https://fonts.googleapis.com/css2?family=Londrina+Solid:wght@300&display=swap" rel="stylesheet">
 </head>
 <body>
+<nav>
+    <ul>
+        <li><a href="/frontend/index.php">Home</a></li>
+        <li><a href="/frontend/login.php">Login</a></li>
+    </ul>
+</nav>
