bd3d754a0430d21fb2f0d0abe446ff274188432c
BrandinGoldsberry <47904796+BrandinGoldsberry@users.noreply.github.com>
2020-08-26 22:30:20 -0600

    Added Basic index and headers

--- frontend/MyHeader.php
+++ frontend/MyHeader.php
@@ -1,3 +1,10 @@
+<?php
+    require "DatabaseConnection.php";
+    if(array_key_exists('userId', $_SESSION)) {
+        $user = $_SESSION['userId'];
+    }
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,3 +13,12 @@
     <link href="https://fonts.googleapis.com/css2?family=Londrina+Solid:wght@300&display=swap" rel="stylesheet">
 </head>
 <body>
+<h1><?php echo $title?></h1>
+<nav>
+    <ul>
+        <li><a href="/PupsNPoodles/frontend/index.php">Home</a></li>
+        <li><a href="/PupsNPoodles/frontend/login.php">Login</a></li>
+        <li><a href="/PupsNPoodles/frontend/appointment.php">Make an Appointment</a></li>
+    </ul>
+    <h1><?php echo $title?></h1>
+</nav>
