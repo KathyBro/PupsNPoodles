ab9e3ede78f3be413d0a3d3071180370103918b1
BrandinGoldsberry <47904796+BrandinGoldsberry@users.noreply.github.com>
2020-08-27 17:05:35 -0600

    Fixed more link errors

--- frontend/MyHeader.php
+++ frontend/MyHeader.php
@@ -23,14 +23,14 @@
             if(isset($_SESSION['userId']))
             {
                 echo $_SESSION['userId'];
-                echo '<li><a href="/frontend/login.php">Logout</a></li>';
+                echo '<li><a href="login.php">Logout</a></li>';
             }
             else
             {
-                echo '<li><a href="/frontend/login.php">Login</a></li>';
+                echo '<li><a href="login.php">Login</a></li>';
             }
         ?>
-        <li><a href="/PupsNPoodles/frontend/appointment.php">Make an Appointment</a></li>
+        <li><a href="appointment.php">Make an Appointment</a></li>
     </ul>
     <h1><?php echo $title?></h1>
 </nav>
